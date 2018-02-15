e6fcb378eb3869b7f001445f01fec188ccc3ff68
javaruban <rubanb.tech@gmail.com>
2018-02-15 12:42:48 +0530

    Created Automation framework folder and file structure

---
+++ EbayAppAutomation/app/src/main/java/com/ebay/automation/MainActivity.java
@@ -0,0 +1,12 @@
+package com.ebay.automation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
